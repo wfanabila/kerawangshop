1d2c1b39da6db9ffb19907eaa0e4cec7d86535cd
qistina <qstnaaa04@gmail.com>
2026-06-11 20:37:26 +0800

    Save my local changes before pulling

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:kerawangshop/login.dart';
 import 'package:lottie/lottie.dart';
-import 'home_screen.dart';
+// import 'home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -34,7 +35,7 @@ class _SplashScreenState extends State<SplashScreen>
 
   void _navigateToHome() {
     Navigator.of(context).pushReplacement(
-      MaterialPageRoute(builder: (_) => const HomeScreen()),
+      MaterialPageRoute(builder: (_) => const Login()),
     );
   }
 
